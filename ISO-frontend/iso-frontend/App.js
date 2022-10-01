5e5cc75f11f4fae9d9e6fb5c4a2852c4cf9689f8
Arjun Taneja <arjun.taneja02@gmail.com>
2022-10-01 15:47:10 -0400

    Remove ugly navigation header

--- ISO-frontend/iso-frontend/App.js
+++ ISO-frontend/iso-frontend/App.js
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Feed" component={Feed} />
       </Stack.Navigator>
     </NavigationContainer>
