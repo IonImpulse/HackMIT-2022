7938deffafa4ad3518872b1933ed90ac4710d811
Arjun Taneja <arjun.taneja02@gmail.com>
2022-10-02 01:57:29 -0400

    Reroute to feed after successfully validating user

--- ISO-frontend/iso-frontend/views/Splash.js
+++ ISO-frontend/iso-frontend/views/Splash.js
@@ -1,6 +1,14 @@
 import { Text, View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Splash = () => {
+    const [userObject, setUserObject] = useState({}); 
+    
+    // Attempt to fetch user object 
+    useEffect(() => {
+        
+    }, [])
 
     return (
         <View>
