a2a015c5216cf56dfbd04918720e5421a279c0ce
Ethan Vazquez <edv121@outlook.com>
2022-10-02 04:01:18 -0400

    format tabs

--- ISO-frontend/iso-frontend/components/HomeWithTabs.js
+++ ISO-frontend/iso-frontend/components/HomeWithTabs.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, TextInput,ScrollView, Keyboard } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Keyboard } from 'react-native';
 import SubmitRequest from '../views/SubmitRequest';
 import Settings from '../views/Settings';
 import MyStuff from '../views/MyStuff';
@@ -13,58 +13,57 @@ const HomeWithTabs = () => {
     const [tab, setTab] = useState("Home");
     const tabPressed = (tab) => {
         setTab(tab);
-        console.log(tab);
-
     }
-    
-  return (
+
+
+    return (
         <SafeAreaView style={styles.biggerContainer}>
             {tab == "Home" && <Feed />}
-            {tab == "Post" && <SubmitRequest/>}
+            {tab == "Post" && <SubmitRequest />}
             {tab == "My Stuff" && <MyStuff />}
             {/* {tab == "Settings" && <RequestDetails><Request type={"ISO"} title={"Testing"} location_string={"Location"} tags={["Food"]}></Request></RequestDetails>} */}
             {tab == "Settings" && <Settings />}
-            
+
             {/* End add component)*/}
 
             <View style={styles.tabBar}>
-                <TouchableOpacity onPress={()=> {tabPressed("Home")}} style={styles.button}>
+                <TouchableOpacity onPress={() => { tabPressed("Home") }} style={tab == "Home" ? styles.buttonPress : styles.button}>
                     <>
-                    <Icon name="home" size={30} color="black" />
-                    <Text>Home</Text>
+                        <Icon name="home" size={30} color="black" />
+                        <Text>Home</Text>
                     </>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={()=> {tabPressed("Post")}} style={styles.button}>
+                <TouchableOpacity onPress={() => { tabPressed("Post") }} style={tab == "Post" ? styles.buttonPress : styles.button}>
                     <>
-                    <Icon name="plus-square-o" size={30} color="black" />
-                    <Text>Post</Text>
+                        <Icon name="plus-square-o" size={30} color="black" />
+                        <Text>Post</Text>
                     </>
                 </TouchableOpacity>
-                    
-                <TouchableOpacity onPress={()=> {tabPressed("My Stuff")}} style={styles.button}>
+
+                <TouchableOpacity onPress={() => { tabPressed("My Stuff") }} style={tab == "My Stuff" ? styles.buttonPress : styles.button}>
                     <>
-                    <Icon name="user-o" size={28} color="black" />
-                    <Text>My Stuff</Text>
+                        <Icon name="user-o" size={30} color="black" />
+                        <Text>My Stuff</Text>
                     </>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=> {tabPressed("Settings")}} style={styles.button}>
+                <TouchableOpacity onPress={() => { tabPressed("Settings") }} style={tab == "Settings" ? styles.buttonPress : styles.button}>
                     <>
-                    <Icon name="gear" size={30} color="black" />
-                    <Text>Settings</Text>
+                        <Icon name="gear" size={30} color="black" />
+                        <Text>Settings</Text>
                     </>
                 </TouchableOpacity>
             </View>
 
         </SafeAreaView>
 
-    
-  )
+
+    )
 }
 
 const styles = StyleSheet.create({
     biggerContainer: {
-        height:"100%",
+        height: "100%",
         width: "100%",
         justifyContent: "center",
     },
@@ -81,14 +80,30 @@ const styles = StyleSheet.create({
     },
     button: {
         marginTop: "2%",
-        marginBottom: "4%",
-        // height: "30%",
-        // width: "20%",
+        marginBottom: "10%",
+        width: "20%",
+        height: "90%",
         backgroundColor: "white",
         alignItems: "center",
         justifyContent: "space-between",
+        paddingTop: 10,
         flexDirection: "column",
-    }
+        paddingBottom: 6,
+    },
+    buttonPress: {
+        marginTop: "2%",
+        marginBottom: "10%",
+        width: "20%",
+        height: "90%",
+        backgroundColor: "#A2D2FF",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: "column",
+        color: "white",
+        borderRadius: 10,
+        paddingTop: 10,
+        paddingBottom: 6,
+    },
 });
 
 export default HomeWithTabs
